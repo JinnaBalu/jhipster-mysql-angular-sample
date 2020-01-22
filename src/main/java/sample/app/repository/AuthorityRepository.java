68cb3095d534064e7f08d2f10a809427171ff95f
Jinna Balu <jinna.balu@wisestep-inc.com>
2020-01-22 23:53:18 +0530

    sample with mysql

---
+++ src/main/java/sample/app/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package sample.app.repository;
+
+import sample.app.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/sample/app/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package sample.app.service.dto;
